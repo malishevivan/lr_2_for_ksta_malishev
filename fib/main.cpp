172b2669515befaf04729f13011f443e3a2ab779
malishevivan <malishev.ivan@list.ru>
2023-11-24 13:58:26 +0300

    merge branches

--- fib/main.cpp
+++ fib/main.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 using namespace std;
+
+int fibbonachi(int N){
+    int ticker = 0;
+    int a = 0, b = 1, nextTerm;
+    cout << "Ryad numbers fib " << N << " iterations: ";
+
+    for(int i = 1; i <= N; i++){
+        if(ticker == 3){
+        cout << a << " ";
+        ticker = 0;
+        }
+        nextTerm = a+b;
+        a = b;
+        b = nextTerm;
+        ticker++;
+    }
+    return 0;
+}
+
 int main() {
-int N;
-cout << "Введите количество итераций N: ";
-cin >> N;
-int a = 0, b = 1, nextTerm;
-cout << "Ряд чисел Фибоначчи до " << N << " итераций: ";
-for (int i = 1; i <= N; ++i) {
-if(i % 2 == 0){
-    cout << a << endl;
-}
-nextTerm = a + b;
-a = b;
-b = nextTerm;
-}
-return 0;
+    int iteration = 0;
+    cout << "Enter N: ";
+    cin >> iteration;
+    fibbonachi(iteration);
 }
